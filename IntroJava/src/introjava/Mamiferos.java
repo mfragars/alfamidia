b2f4b671e96cbdfac679b1f7440c191374d921c6
noite <noite@I5-2011-2.aluno.local>
2015-08-05 21:03:00 -0300

    Exercicio

---
+++ IntroJava/src/introjava/Mamiferos.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package introjava;
+
+/**
+ *
+ * @author noite
+ */
+public class Mamiferos {
+
+    private String corDosPelos = null;
+    private String comprimentoDosPelos;
+
+    public void Sentar() {
+        
+    }
+
+    public void Amamentar() {
+
+    }
+}
