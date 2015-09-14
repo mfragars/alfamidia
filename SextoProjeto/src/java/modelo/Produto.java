d9f0139d6d44854f8c479e5fa58cfbb92ecaec7c
noite <noite@I5-2013-08.aluno.local>
2015-09-14 19:30:28 -0300

    Aula 14/09

---
+++ SextoProjeto/src/java/modelo/Produto.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author noite
+ */
+public class Produto {
+    private int id;
+    private String nome;
+    private String descricao;
+    private double valor;
+    
+    public Produto(){
+        
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+    
+    public String toString(){
+        return "id: ".concat(this.id+" Nome: "+this.nome+" Descrição: "+this.descricao+" Preço: "+this.valor);
+    }
+    
+}
